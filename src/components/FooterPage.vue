bcea663f6c6ec1afa03adeda7c3f738c6bf22d33
VincenzoZ24 <zitov287@gmail.com>
2022-11-24 15:48:42 +0100

    settaggio

---
+++ src/components/FooterPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    bho
+  </footer>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    bho
+  </main>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+
+</style>
