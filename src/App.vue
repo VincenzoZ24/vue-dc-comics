fe3cd8bb0de15db5850d0b54a80484b73aaa1290
VincenzoZ24 <zitov287@gmail.com>
2022-11-24 16:36:55 +0100

    nav completa

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
--- src/components/MenuPage.vue
+++ src/components/MenuPage.vue
@@ -1,7 +1,17 @@
 <template>
   <nav>
-    <ul>
-      <li></li>
+    <img class="logo" src="@/img/dc-logo.png" alt="">
+    <ul class="list">
+      <li>CHARACTERS</li>
+      <li>COMICS</li>
+      <li>MOVIES</li>
+      <li>TV</li>
+      <li>GAMES</li>
+      <li>COLLECTIBLES</li>
+      <li>VIDEOS</li>
+      <li>FANS</li>
+      <li>NEWS</li>
+      <li>SHOP</li>
     </ul>
   </nav>
 </template>
@@ -14,5 +24,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.logo{
+  width: 70px;
+  object-fit: contain;
+}
+nav{
+  max-width: 1200px;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem;
+}
+.list{
+  display: flex;
+  gap: 1rem;
+  li{
+    list-style: none;
+  }
+}
 </style>
