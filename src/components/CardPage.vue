1eb13ab2da0039e3dc0d70b6f32043137b2b3a3e
VincenzoZ24 <zitov287@gmail.com>
2022-11-25 16:15:22 +0100

    creato cards

---
+++ src/components/CardPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <div class="contents">
+      <img class="img_card" :src="img" alt="" />
+      <h5>{{ title }}</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardPage',
+  props: {
+    img: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.contents{
+    width: calc((1200px - 11rem) / 6);
+    color: white;
+    margin-bottom: 2rem;
+}
+img{
+    width: 100%;
+}
+</style>
